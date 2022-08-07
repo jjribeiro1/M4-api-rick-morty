569ddfbedfa05ffda2bddb69b5388c8c2bd0ebf4
jvsr18 <jvsr.99@hotmail.com>
2022-08-07 15:18:43 -0300

    chore: importando a rota dos personagens

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
 const app = express();
+const charactersRoute = require("./routes/characters.route")
 const dbConnection = require("./database/db");
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/characters", charactersRoute)
 
 dbConnection();
 
