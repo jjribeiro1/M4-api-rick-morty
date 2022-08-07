0b86c692c966eecb17c4fc4f0c7cd194c1732418
jvsr18 <jvsr.99@hotmail.com>
2022-08-07 15:17:10 -0300

    feat(routes): definição das rotas de criação e leitura dos personagens

---
+++ src/routes/characters.route.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const charactersController = require("../controllers/characters.controller");
+
+router.post("/create", charactersController.createCharacterController);
+router.get("/", charactersController.readAllCharacterController);
+router.get("/:id", charactersController.readCharacterByIdController);
+
+
+module.exports = router;
